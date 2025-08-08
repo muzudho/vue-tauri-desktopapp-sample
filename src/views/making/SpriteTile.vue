6021aff5f09c5f84e28e3781d51d17261ade9eec
muzudho <muzudho1@gmail.com>
2025-08-08 23:30:23 +0900

    TODO: SpriteTile練習

--- src/views/making/SpriteTile.vue
+++ src/views/making/SpriteTile.vue
@@ -1,8 +1,27 @@
 <template>
     <h2>## スプライトの練習、まずタイルを切り抜くぜ！</h2>
     <section class="sec-2">
-        TODO ここにタイル表示
+        TODO ここにタイル表示：
+        <Tile
+                :srcLeft="64"
+                :srcTop="32"
+                :srcWidth="32"
+                :srcHeight="64"
+                :tilemapUrl="'/img/tilemap_sample.png'" />
+        ここまで。
+
     </section>
     <hr/>
     <router-link to="/making">メイキングに戻る</router-link>
 </template>
+
+<script setup lang="ts">
+
+    //import Tile from '@/components/Tile.vue';
+    import Tile from '../../components/Tile.vue';
+
+    // ##############
+    // # インポート #
+    // ##############
+
+</script>
