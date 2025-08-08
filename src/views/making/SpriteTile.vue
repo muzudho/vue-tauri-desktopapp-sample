fac2d7efa43bddb10c6503a0042b183db069251f
muzudho <muzudho1@gmail.com>
2025-08-08 23:46:28 +0900

    SpriteTile練習

--- src/views/making/SpriteTile.vue
+++ src/views/making/SpriteTile.vue
@@ -1,14 +1,18 @@
 <template>
     <h2>## スプライトの練習、まずタイルを切り抜くぜ！</h2>
     <section class="sec-2">
-        TODO ここにタイル表示：<br/>
+        ここに切り抜いたタイルを表示：<br/>
         <Tile
-                :srcLeft="64"
-                :srcTop="32"
-                :srcWidth="32"
-                :srcHeight="64"
-                :tilemapUrl="'/img/tilemap_sample.png'" /><br/>
-        ここまで。
+                srcLeft="64"
+                srcTop="32"
+                srcWidth="32"
+                srcHeight="64"
+                tilemapUrl="/img/tilemap_sample.png" /><br/>
+        ：ここまで。<br/>
+        <br/>
+        元画像のタイルマップを表示：<br/>
+        <v-img src="/img/tilemap_sample.png" style="width:128px; height:128px;"/>
+        ：ここまで。
     </section>
     <hr/>
     <router-link to="/making">メイキングに戻る</router-link>
@@ -20,7 +24,6 @@
     // # インポート #
     // ##############
 
-    //import Tile from '../../components/Tile.vue';
     import Tile from '@/components/Tile.vue';
 
 </script>
