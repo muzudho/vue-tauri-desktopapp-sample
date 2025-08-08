b60b3932923d60d583b0a83965e1e94bbb59ca1f
muzudho <muzudho1@gmail.com>
2025-08-08 15:30:34 +0900

    WIP: ページレイアウト仕様変更中

--- src/views/PageLayout.vue
+++ src/views/PageLayout.vue
@@ -1,42 +1,42 @@
 <template>
     <v-container>
-        <!-- 上は［巻］＞［編］ -->
-        <h1>部（1 Part）の見出し</h1>
-        <h2>章（2 Chapter）の見出し</h2>
-        <h3>節（3 Section）の見出し</h3>
-        <h4>項（4 Paragraph）の見出し</h4>
-        <h5>条（5 Article）の見出し</h5>
-        <h6>段（6）の見出し</h6><!-- ６段目とか日本語に無い。条と段の順序は同じぐらい -->
+        <!-- 上は［巻］＞［編］＞［部］ -->
+        <h1>章（1 Chapter）の見出し</h1>
+        <h2>節（2 Section）の見出し</h2>
+        <h3>項（3 Paragraph）の見出し</h3>
+        <h4>条（4 Article）の見出し</h4>
+        <h5>段（5）の見出し</h5><!-- ５段目とか日本語に無い。条と段の順序は同じぐらい -->
+        <h6>小見出し（6 Subheader）の見出し</h6><!-- ６段目とか日本語に無い。 -->
     </v-container>
     <v-container>
-        <h1>１部</h1>
+        <h1>１章</h1>
         <section class="sec-1">
-            １部の本文だぜ（＾▽＾）！
-        </section>
-
-        <h2>１章</h2>
-        <section class="sec-2">
             １章の本文だぜ（＾▽＾）！
         </section>
 
-        <h3>１節</h3>
-        <section class="sec-3">
+        <h2>１節</h2>
+        <section class="sec-2">
             １節の本文だぜ（＾▽＾）！
         </section>
 
-        <h4>１項</h4>
-        <section class="sec-4">
+        <h3>１項</h3>
+        <section class="sec-3">
             １項の本文だぜ（＾▽＾）！
         </section>
 
-        <h5>１条</h5>
-        <section class="sec-5">
+        <h4>１条</h4>
+        <section class="sec-4">
             １条の本文だぜ（＾▽＾）！
         </section>
 
-        <h6>１段</h6>
-        <section class="sec-6">
+        <h5>１段</h5>
+        <section class="sec-5">
             １段の本文だぜ（＾▽＾）！
+        </section>
+
+        <h6>１小見出し</h6>
+        <section class="sec-6">
+            １小見出しの本文だぜ（＾▽＾）！
         </section>
 
 </v-container>
