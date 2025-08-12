e812fe79456fbe94c89713f4d46c1b123837fe67
muzudho <muzudho1@gmail.com>
2025-08-13 00:15:04 +0900

    WIP: スプライト作成途中

--- src/pages/making/sprite__img.vue
+++ src/pages/making/sprite__img.vue
@@ -3,10 +3,19 @@
 
     <h3>矢印キーで移動しようぜ！</h3>
     <section class="sec-3">
+        <!-- キャンバスは画面みたいなもの。 -->
         <p>矢印キーで移動！</p>
-        <div
-            class="cursor"
-            :style="walkGraphicStyle"></div>
+        <img
+            src="/img/making/202508__warabenture__12--2149-kifuwarabe-o1o0.png"
+            :style="walkGraphicStyle"
+            style="position: relative;"></img><br/>
+        <canvas
+            id="myCanvas"
+            ref="myCanvas"
+            width="400"
+            height="400"
+            ></canvas>
+        
     </section>
 </template>
 
@@ -27,6 +36,9 @@
     // ##############
     // # 共有データ #
     // ##############
+
+    // https://developer.mozilla.org/ja/docs/Web/API/HTMLCanvasElement
+    const myCanvas = ref<HTMLCanvasElement | null>(null); // Canvas要素を参照
 
     const x = ref<number>(0);  // スプライトのX座標
     const y = ref<number>(0);  // スプライトのY座標
@@ -52,6 +64,12 @@
     // ##########
 
     onMounted(() => {
+        // Canvasとコンテキストを取得
+        myCanvas.value = document.getElementById('myCanvas') as HTMLCanvasElement;
+
+        // // https://developer.mozilla.org/ja/docs/Web/API/HTMLCanvasElement/getContext
+        // const ctx : CanvasRenderingContext2D | null = myCanvas.value.getContext('2d');
+
         // スプライト画像の読み込み
         sprite.src = "/img/making/202508__warabenture__12--2149-kifuwarabe-o1o0.png";
         sprite.onload = () => {
@@ -93,6 +111,16 @@
                     x.value += speed.value;
                 }
 
+                // 画面外に出ないように制限
+                // x.value = Math.max(0, Math.min(x.value, myCanvas.value?.width ?? 1 - 32));
+                // y.value = Math.max(0, Math.min(y.value, myCanvas.value?.height ?? 1 - 32));
+
+                // // 描画
+                // if (ctx != null){
+                //     ctx.clearRect(0, 0, myCanvas.value?.width ?? 1, myCanvas.value?.height ?? 1);
+                //     ctx.drawImage(sprite, x.value, y.value);
+                // }
+
                 // 次のフレーム
                 requestAnimationFrame(update);
             };
@@ -106,7 +134,7 @@
 </script>
 
 <style scoped>
-    div.cursor {
-        position: relative; border:dashed 4px green; width:32px; height:32px;
+    canvas {
+        border: 1px solid black;
     }
 </style>
