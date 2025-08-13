4656336534f81e99125276ed078efe82d6a1a2e1
muzudho <muzudho1@gmail.com>
2025-08-13 18:47:26 +0900

    WIP: 戻るリンクをボタンに変更中

--- src/pages/making/the-header.vue
+++ src/pages/making/the-header.vue
@@ -1,6 +1,19 @@
 <template>
     <h2>メイキングの章だぜ！</h2>
     <section class="sec-2">
-        <router-link to="/making">戻る</router-link>
+        <v-btn @click="goToMaking">下の節を閉じる</v-btn>
+        <!--
+            <router-link to="/making">戻る</router-link>
+        -->
     </section>
 </template>
+
+<script setup lang="ts">
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    function goToMaking() : void {
+        router.push('/making');
+    }
+</script>
