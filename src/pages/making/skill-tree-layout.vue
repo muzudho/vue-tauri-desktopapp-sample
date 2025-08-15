9cfdf51a4561fd4f394956df6433bdea16bdf679
muzudho <muzudho1@gmail.com>
2025-08-15 17:48:23 +0900

    WIP: 矢印練習

--- src/pages/making/skill-tree-layout.vue
+++ src/pages/making/skill-tree-layout.vue
@@ -2,27 +2,29 @@
     <the-header/>
 
     <h3>説明を始める前に、スキル・ツリーのレイアウトを決めておこうぜ！</h3>
-    <section class="sec-3" style="position: relative; left: 0; top: 0; height: 256px;">
-        凡例だぜ（＾▽＾）！
+    <section class="sec-3" style="position: relative;">
+        <div style="position: absolute; height: 256px;">
+            凡例だぜ（＾▽＾）！
 
-        <div class="d-flex align-center justify-center" style="position:absolute; border:solid 4px brown; width:256px; height:64px;">
-            初期スキル
+            <div class="rectangle d-flex align-center justify-center" style="position:absolute; border:solid 4px brown; width:256px; height:64px;">
+                初期スキル
+            </div>
+            <Arrow
+                :x1="0"
+                :y1="64"
+                :x2="150"
+                :y2="100"
+                :stroke-width="8"
+                color="blue"/>
+
+            <Arrow
+                :x1="0"
+                :y1="100"
+                :x2="150"
+                :y2="200"
+                :stroke-width="8"
+                color="blue"/>
         </div>
-        <Arrow
-            :x1="0"
-            :y1="0"
-            :x2="150"
-            :y2="100"
-            :stroke-width="8"
-            color="blue"/>
-
-        <Arrow
-            :x1="0"
-            :y1="100"
-            :x2="150"
-            :y2="200"
-            :stroke-width="8"
-            color="blue"/>
     </section>
 
     <the-footer/>
