ac3cf44faee4426d45d1c00a3cfb06dfbdae120f
muzudho <muzudho1@gmail.com>
2025-08-15 23:23:10 +0900

    WIP: 矢印2練習

--- src/pages/making/svg-arrow.vue
+++ src/pages/making/svg-arrow.vue
@@ -11,7 +11,7 @@
 
             <br/>
             <v-checkbox
-                v-for="index in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]"
+                v-for="index in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]"
                 :key="index"
                 v-model="hitboxCheckboxes"
                 :value="`${index}`"
@@ -19,8 +19,9 @@
                 ></v-checkbox>
 
             <div
-                v-for="index in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]" :key="index"
-                :style="hitboxStyles[index]">{{ index }}</div>
+                v-for="index in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]" :key="index"
+                :style="hitboxPositions[index] + ' ' +  hitboxStyles[index]"
+                style="position: absolute; border: solid 4px black; z-index: 10;">{{ index }}</div>
 
             <Arrow
                 :startX="10 * cellWidth"
@@ -182,26 +183,36 @@
     const cellHeight = 32;
 
     // 当たり判定
+    const startX = 5;
+    const startY = 5;
     const hitboxCheckboxes = ref<string[]>([]);
-    const hitboxStyles = ref<string[]>(["", "", "", "", "", "", "", "", "", "", "", "", ""]);
+    const hitboxStyles = ref<string[]>(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]);
+    const hitboxPositions : string[] = [
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(0   + 1  ) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 0
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 1
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 2
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  2.5 + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 3
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(0   + 1  ) * cellHeight}px;`,    // 4
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 5
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 6
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 7
+        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(0   + 1  ) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 8
+        `left: ${( 2.5 + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 9
+        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 10
+        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 11
+        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(0   + 1  ) * cellHeight}px;`,    // 12
+        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  2.5 + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 13
+        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 14
+        `left: ${( 2.5 + startX      ) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 15
+    ];
     watch(hitboxCheckboxes, (newValue:string[]) => {
         console.log(`newValue: ${newValue}`);
 
-        for (let i = 0; i < 12; i++) {
+        for (let i = 0; i < hitboxPositions.length; i++) {
             if (newValue.includes(`${i}`)) {
-                hitboxStyles.value[i] = `
-                    visibility: visible;
-                    position: relative;
-                    left: ${9.5 * cellWidth}px;
-                    top: ${5 * cellHeight}px;
-                    width: ${1 * cellWidth}px;
-                    height: ${5 * cellHeight}px;
-                    border: solid 4px black;
-                    background-color: blue;
-                `;
+                hitboxStyles.value[i] = `visibility: visible;`;
             } else {
-                hitboxStyles.value[i] = "position: relative; background-color:red; width:120px; height:30px;";
-                //hitboxStyles.value[i] = "visibility: hidden;";
+                hitboxStyles.value[i] = "visibility: hidden;";
             }
         }
     });
