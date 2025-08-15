7005e597da3852735f6f76cd668cab91631d997d
muzudho <muzudho1@gmail.com>
2025-08-15 23:43:40 +0900

    WIP: 矢印2練習

--- src/pages/making/svg-arrow.vue
+++ src/pages/making/svg-arrow.vue
@@ -18,128 +18,128 @@
                 style="position: absolute; border: solid 4px lightgray;">{{ index }}</div>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="0 * cellWidth"
                 :height="-5 * cellHeight"
                 :stroke-width="8"
                 color="#ff0000"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="2.5 * cellWidth"
                 :height="-5 * cellHeight"
                 :stroke-width="8"
                 color="#ff4000"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="5 * cellWidth"
                 :height="-5 * cellHeight"
                 :stroke-width="8"
                 color="#ff8000"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="5 * cellWidth"
                 :height="-2.5 * cellHeight"
                 :stroke-width="8"
                 color="#ffc000"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="5 * cellWidth"
                 :height="0 * cellHeight"
                 :stroke-width="8"
                 color="#ffff00"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="5 * cellWidth"
                 :height="2.5 * cellHeight"
                 :stroke-width="8"
                 color="#c0ff00"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="5 * cellWidth"
                 :height="5 * cellHeight"
                 :stroke-width="8"
                 color="#80ff00"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="2.5 * cellWidth"
                 :height="5 * cellHeight"
                 :stroke-width="8"
                 color="#40ff00"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="0 * cellWidth"
                 :height="5 * cellHeight"
                 :stroke-width="8"
                 color="#00ff00"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-2.5 * cellWidth"
                 :height="5 * cellHeight"
                 :stroke-width="8"
                 color="#00ff80"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-5 * cellWidth"
                 :height="5 * cellHeight"
                 :stroke-width="8"
                 color="#00ffff"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-5 * cellWidth"
                 :height="2.5 * cellHeight"
                 :stroke-width="8"
                 color="#0080ff"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-5 * cellWidth"
                 :height="0 * cellHeight"
                 :stroke-width="8"
                 color="#0000ff"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-5 * cellWidth"
                 :height="-2.5 * cellHeight"
                 :stroke-width="8"
                 color="#8000ff"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-5 * cellWidth"
                 :height="-5 * cellHeight"
                 :stroke-width="8"
                 color="#ff00ff"/>
 
             <Arrow
-                :startX="10 * cellWidth"
-                :startY="10 * cellHeight"
+                :startX="startX * cellWidth"
+                :startY="startY * cellHeight"
                 :width="-2.5 * cellWidth"
                 :height="-5 * cellHeight"
                 :stroke-width="8"
@@ -191,22 +191,22 @@
     const hitboxCheckboxes = ref<string[]>([]);
     const hitboxStyles = ref<string[]>(["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]);
     const hitboxPositions : string[] = [
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(0   + 1  ) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 0
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 1
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 2
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  2.5 + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 3
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(0   + 1  ) * cellHeight}px;`,    // 4
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 5
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 6
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 7
-        `left: ${( 5   + startX - 0.5) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(0   + 1  ) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 8
-        `left: ${( 2.5 + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 9
-        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 10
-        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 11
-        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  5   + startY - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(0   + 1  ) * cellHeight}px;`,    // 12
-        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  2.5 + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 13
-        `left: ${( 0   + startX      ) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 14
-        `left: ${( 2.5 + startX      ) * cellWidth}px; top: ${(  0   + startY + 0  ) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 15
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 5   + 0  ) * cellHeight}px; width: ${(0   + 1  ) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 0
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 5   + 0  ) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 1
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 5   + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 2
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 2.5 + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 3
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(0   + 1  ) * cellHeight}px;`,    // 4
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 5
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 6
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 7
+        `left: ${( startX + 0   - 0.5) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(0   + 1  ) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 8
+        `left: ${( startX - 2.5      ) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 9
+        `left: ${( startX - 5        ) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 10
+        `left: ${( startX - 5        ) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 11
+        `left: ${( startX - 5        ) * cellWidth}px; top: ${(  startY - 0   - 0.5) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(0   + 1  ) * cellHeight}px;`,    // 12
+        `left: ${( startX - 5        ) * cellWidth}px; top: ${(  startY - 2.5 + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(2.5 + 0.5) * cellHeight}px;`,    // 13
+        `left: ${( startX - 5        ) * cellWidth}px; top: ${(  startY - 5   + 0  ) * cellHeight}px; width: ${(5   + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 14
+        `left: ${( startX - 2.5      ) * cellWidth}px; top: ${(  startY - 5   + 0  ) * cellHeight}px; width: ${(2.5 + 0.5) * cellWidth}px; height: ${(5   + 0.5) * cellHeight}px;`,    // 15
     ];
     watch(hitboxCheckboxes, (newValue:string[]) => {
         console.log(`newValue: ${newValue}`);
