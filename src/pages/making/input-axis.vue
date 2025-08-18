32bac8554a07875822d0fbf8e0db1425e3af0831
muzudho <muzudho1@gmail.com>
2025-08-19 03:28:09 +0900

    Rename files

--- src/pages/making/input-axis.vue
+++ src/pages/making/input-axis.vue
@@ -8,11 +8,11 @@
     </section>
 
     <input-axis-target/><br/>
-    <input-axis-rpg-walk/><br/>
-    <input-axis-rpg-walk-grid/><br/>
-    <input-axis-rpg-walk-boundary-check/><br/>
-    <input-axis-rpg-walk-scroll-primordial/><br/>
-    <input-axis-rpg-walk-scroll-loop/><br/>
+    <input-axis-rpg-walk-player-primordial/><br/>
+    <input-axis-rpg-walk-player-grid/><br/>
+    <input-axis-rpg-walk-player-boundary-check/><br/>
+    <input-axis-rpg-walk-board-scroll-primordial/><br/>
+    <input-axis-rpg-walk-board-scroll-loop/><br/>
     <input-axis-rpg-walk-face-shift-primordial/><br/>
     <input-axis-rpg-walk-face-shift-boundary/><br/>
     <input-axis-rpg-walk-face-shift-loop/><br/>
@@ -42,16 +42,16 @@
     //
 
     import SourceLink from '../../components/SourceLink.vue';
-    import InputAxisRpgWalk from './input-axis-rpg-walk.vue';
-    import InputAxisRpgWalkBoundaryCheck from './input-axis-rpg-walk-boundary-check.vue';
-    import InputAxisRpgWalkGrid from './input-axis-rpg-walk-grid.vue';
+    import InputAxisRpgWalkPlayerPrimordial from './input-axis-rpg-walk-player-primordial.vue';
+    import InputAxisRpgWalkPlayerBoundaryCheck from './input-axis-rpg-walk-player-boundary-check.vue';
+    import InputAxisRpgWalkPlayerGrid from './input-axis-rpg-walk-player-grid.vue';
     import InputAxisRpgWalkScrollBackground from './input-axis-rpg-walk-scroll-background.vue';
-    import InputAxisRpgWalkScrollLoop from './input-axis-rpg-walk-scroll-loop.vue';
+    import InputAxisRpgWalkBoardScrollLoop from './input-axis-rpg-walk-board-scroll-loop.vue';
     import InputAxisRpgWalkFaceShiftPrimordial from './input-axis-rpg-walk-face-shift-primordial.vue';
     import InputAxisRpgWalkFaceShiftBoundary from './input-axis-rpg-walk-face-shift-boundary.vue';
     import InputAxisRpgWalkFaceShiftContentsSizeVariable from './input-axis-rpg-walk-face-shift-contents-size-variable.vue';
     import InputAxisRpgWalkFaceShiftLoop from './input-axis-rpg-walk-face-shift-loop.vue';
-    import InputAxisRpgWalkScrollPrimordial from './input-axis-rpg-walk-scroll-primordial.vue';
+    import InputAxisRpgWalkBoardScrollPrimordial from './input-axis-rpg-walk-board-scroll-primordial.vue';
     import InputAxisTarget from './input-axis-target.vue';
     import TheFooter from './the-footer.vue';
     import TheHeader from './the-header.vue';
