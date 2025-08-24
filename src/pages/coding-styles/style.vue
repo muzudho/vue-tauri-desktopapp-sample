58e3a8a3576c320a68470d66e82200743a26ae12
muzudho <muzudho1@gmail.com>
2025-08-24 22:13:52 +0900

    WIP: コーディング・スタイルの部

--- src/pages/coding-styles/style.vue
+++ src/pages/coding-styles/style.vue
@@ -3,46 +3,66 @@
 
     <h3>スタイルの書き方を決めておこうぜの章だぜ！</h3>
     <section class="sec-3">
-        <p>例えば、青い枠を描くとき：</p>
+        <p>
+            使っているのは Vue3 と Vuetify3。<br/>
+            <br/>
+            例えば、青い長方形を描くとき：
+        </p>
         <br/>
 
         <div
             style="
-                width: 100px;
+                width: 200px;
                 height: 100px;
                 background-color: blue;
             ">
         </div>
         <br/>
 
-        <pre
-            style="
-                display: inline-block;
-                border: dashed 4px gray;
-                background-color: #f0f0f0;
-                padding-left: 24px;
-                padding-top: 16px;
-                padding-right: 24px;
-            "
-        >
+        📄 example.vue ：<br/>
+        <pre class="coding-example">
 &lt;template&gt;
     &lt;div
-        style=<span style="color: brown; font-weight: bolder;">"</span>
-            <span style="color: brown; font-weight: bolder;">width: 100px;</span>
-            <span style="color: brown; font-weight: bolder;">height: 100px;</span>
-            <span style="color: brown; font-weight: bolder;">background-color: blue;</span>
-        <span style="color: brown; font-weight: bolder;">"</span>&gt;
+        style=<span class="em">"
+            width: 100px;
+            height: 200px;
+            background-color: blue;
+        "</span>&gt;
     &lt;/div&gt;
 &lt;/template&gt;
         </pre>
-        <p>👆　上記のように書くことができる。これがインライン・スタイル。</p>
+        <p>
+            👆　上記のように書くことができる。これがインライン・スタイル。<br/>
+            インラインは本当は１行に書くことだが、細かいことは気にしない。<br/>
+
+        </p>
+        <br/>
 
         <p>以下のようにも書ける：</p>
         <br/>
 
-        <div
-            class="blue-rectangle">
+        <div class="blue-rectangle">
         </div>
+        <br/>
+
+        📄 example.vue ：<br/>
+        <pre class="coding-example">
+&lt;template&gt;
+    &lt;div <span class="em">class="blue-rectangle"</span>&gt;
+    &lt;/div&gt;
+&lt;/template&gt;
+
+&lt;style scoped&gt;
+    <span class="em">div.blue-rectangle {    /* 青い長方形 */
+        width: 200px;
+        height: 100px;
+        background-color: blue;
+    }</span>
+&lt;/style&gt;
+        </pre>
+        <p>
+            👆　これはスタイルシートを使った書き方。<br/>
+        </p>
         <br/>
 
     </section>
@@ -76,8 +96,22 @@
 </script>
 
 <style scoped>
-    div.blue-rectangle {
-        width: 100px;
+    pre.coding-example {    /* ソースコード例 */
+        display: inline-block;
+        border: dashed 4px gray;
+        background-color: #f0f0f0;
+        padding-left: 24px;
+        padding-top: 16px;
+        padding-right: 24px;
+    }
+
+    span.em {   /* 強調 */
+        color: brown;
+        font-weight: bolder;
+    }
+
+    div.blue-rectangle {    /* 青い長方形 */
+        width: 200px;
         height: 100px;
         background-color: blue;
     }
