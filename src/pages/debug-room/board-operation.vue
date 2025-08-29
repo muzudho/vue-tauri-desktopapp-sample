ef98e0fb3026d29061c0fb69ee0ddee514470796
muzudho <muzudho1@gmail.com>
2025-08-29 23:01:15 +0900

    WIP: デバッグ・ルーム準備

--- src/pages/debug-room/board-operation.vue
+++ src/pages/debug-room/board-operation.vue
@@ -3,6 +3,29 @@
 
     <h3>盤操作のデバッグだぜ！</h3>
     <section class="sec-3">
+        <pre>
+getFixedSquareIndexFromTileIndex(
+    tileIndex,
+    board1SquareWidth,
+    board1SquareHeight,
+    board1FileNum,
+    board1RankNum,
+    printing1Left,
+    printing1Top,
+)</pre>
+        = {{ 
+            getFixedSquareIndexFromTileIndex(
+                tileIndex,
+                board1SquareWidth,
+                board1SquareHeight,
+                board1FileNum,
+                board1RankNum,
+                printing1Left,
+                printing1Top,
+            )
+        }}
+
+
     </section>
 
     <br/>
@@ -20,6 +43,9 @@
     // # インポート #
     // ##############
 
+    import { ref } from 'vue';
+    // 👆 ［初級者向けのソースコード］では、 reactive は使いません。
+
     // ++++++++++++++++++
     // + コンポーネント +
     // ++++++++++++++++++
@@ -30,5 +56,42 @@
     import SourceLink from '../../components/SourceLink.vue';
     import TheFooter from './the-footer.vue';
     import TheHeader from './the-header.vue';
+
+    // ++++++++++++++++++
+    // + コンポーザブル +
+    // ++++++++++++++++++
+
+    import { getFixedSquareIndexFromTileIndex } from '../../composables/board-operation';
+
+
+    // ################
+    // # オブジェクト #
+    // ################
+
+    // ++++++++++++++++++++++++++++++++++
+    // + オブジェクト　＞　テストデータ +
+    // ++++++++++++++++++++++++++++++++++
+
+    const tileIndex = 18;
+
+    // ++++++++++++++++++++++++
+    // + オブジェクト　＞　盤 +
+    // ++++++++++++++++++++++++
+
+    const board1SquareWidth = 32;
+    const board1SquareHeight = 32;
+    const board1FileNum = ref<number>(9);   // 筋の数。ただし、右側と下側に１マス余分に付いているマスクは含まない。
+    const board1RankNum = ref<number>(9);   // 段の数
+
+    // ++++++++++++++++++++++++++
+    // + オブジェクト　＞　印字 +
+    // ++++++++++++++++++++++++**
+    //
+    // 盤上に表示される数字柄、絵柄など。
+    //
+
+    // のちのち自機を１ドットずつ動かすことを考えると、 File, Rank ではデジタルになってしまうので、 Left, Top で指定したい。
+    const printing1Left = ref<number>(0);
+    const printing1Top = ref<number>(0);
 
 </script>
