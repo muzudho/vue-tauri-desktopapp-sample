8ecf42f1cbba1202ae50f2e89b8b0a2c43307ac3
muzudho <muzudho1@gmail.com>
2025-08-30 10:56:47 +0900

    自機と印字の分離

--- src/composables/printing-controller.ts
+++ src/composables/printing-controller.ts
@@ -98,3 +98,199 @@ export function printingMotionClearIfCountZero(
         printing1Motion.value.wrapAroundBottom = 0;
     }
 }
+
+
+/**
+ * 画面端が見えるような動きかをチェックします。
+ */
+export function checkOutOfSightLeftIsLook(
+    board1SquareWidth: number,
+    board1WithMaskSizeSquare: number,
+    printing1Left: number,
+) : boolean {
+    // 見えている画面外が広がるような移動は禁止する：
+    //
+    // - 印字は動かない、プレイヤーの移動量を見ること。
+    // TODO: プレイヤーの移動量と、印字の移動量を分けれないか？
+    //
+    // Printing
+    // +---------------------+
+    // |                     |
+    // |       Board         |
+    // |       +-------+     |
+    // |       |       |     |
+    // |       |   *   |     |
+    // |       |       |     |
+    // |       +-------+     |
+    // |                     |
+    // +<--m-->*-------------+
+    //         0
+    // c<------+
+    // pd
+    //
+    // 0 は、盤の初期位置からの移動量。盤は移動しないので常に 0。
+    // c は、0 からみた、印字の左隅位置。 初期値は習慣的に、 0 以下にするものと思われる。
+    // m は、 pd の正負を反転したもの。
+    // m が、マスク幅より大きいなら、それ以上左に行くことはできない。
+    //
+    // m = c
+    //
+
+    const pd = getPrinting1FileDelta(printing1Left, board1SquareWidth) - 1;  // まだ -1 （左へ移動）されていないので、-1 しておく。
+    const m = - pd;
+
+    return board1WithMaskSizeSquare < m;
+}
+
+
+/**
+ * 画面端が見えるような動きかをチェックします。
+ */
+export function checkOutOfSightTopIsLook(
+    board1SquareHeight: number,
+    board1WithMaskSizeSquare: number,
+    printing1Top: number,
+) : boolean {
+    // 見えている画面外が広がるような移動は禁止する：
+    //
+    // - 印字は動かない、プレイヤーの移動量を見ること。
+    // TODO: プレイヤーの移動量と、印字の移動量を分けれないか？
+    //
+    // Printing
+    // +-------------------->+     c  pd
+    // |                     |     ^
+    // |       Board         |     |
+    // |       +-------+     |     +
+    // |       |       |     |
+    // |       |       |     |
+    // |       |   *   |     |
+    // |       |       |     |
+    // |       |       |     |
+    // |       +-------+     +
+    // |                     ^
+    // |                     |
+    // |                     m
+    // |                     |
+    // |                     v
+    // +---------------------+
+    //
+    // 0 は、盤の初期位置からの移動量。盤は移動しないので常に 0。
+    // c は、0 からみた、印字の上隅位置。 初期値は習慣的に、 0 以下にするものと思われる。
+    // m は、 pd の正負を反転したもの。
+    // m が、マスク幅より大きいなら、それ以上上に行くことはできない。
+    //
+    // m = c
+    //
+
+    const pd = getPrinting1RankDelta(printing1Top, board1SquareHeight) - 1;  // まだ -1 （上へ移動）されていないので、-1 しておく。
+    const m = - pd;
+    return board1WithMaskSizeSquare < m;
+}
+
+
+/**
+ * 画面端が見えるような動きかをチェックします。
+ */
+export function checkOutOfSightRightIsLook(
+    board1SquareWidth: number,
+    board1WithMaskSizeSquare: number,
+    board1FileNum: number,
+    printing1FileNum: number,
+    printing1Left: number,
+) : boolean {
+    // 見えている画面外が広がるような移動は禁止する：
+    //
+    // - 印字は動かない、プレイヤーの移動量を見ること。
+    // TODO: プレイヤーの移動量と、印字の移動量を分けれないか？
+    //
+    // Printing
+    // +<---------cw------------------->+
+    // |                                |
+    // |       Board                    |
+    // |       +<------bw-------->+     |
+    // |       |//////////////////|     |
+    // |       |///+-------+//////|     |
+    // |       |///|       |//////|     |
+    // |       |///|   *   |//////|     |
+    // |       |///|       |//////|     |
+    // |       |///+-------+//////|     |
+    // |       |//////////////////|     |
+    // |       |//////////////////|     |
+    // |       +------------------+     |
+    // |                                |
+    // +--------------------------+<-m->+
+    // c<------+
+    // pd
+    //
+    // 0 は、盤の初期位置からの移動量。盤は移動しないので常に 0。
+    // c は、0 からみた、印字の左隅位置。 初期値は習慣的に、 0 以下にするものと思われる。
+    // bw は、盤の列数。
+    // cw は、印字の列数。
+    // m は、右側番外の余白。
+    // m が、マスクの横幅（右側の多めの１を含まない）以下なら、それ以上右に行くことはできない。
+    //
+    // m = cw + c - bw
+    //
+
+    const pd = -(getPrinting1FileDelta(printing1Left, board1SquareWidth) + 1);  // まだ 1 （右へ移動）されていないので、1 しておく。
+    const cw = printing1FileNum; // 例えば 10
+    const bw = board1FileNum;
+    const m = cw + pd - bw;
+    return m < -board1WithMaskSizeSquare;
+}
+
+
+/**
+ * 画面端が見えるような動きかをチェックします。
+ */
+export function checkOutOfSightBottomIsLook(
+    board1SquareHeight: number,
+    board1WithMaskSizeSquare: number,
+    board1RankNum: number,
+    printing1RankNum: number,
+    printing1Top: number,
+) : boolean {
+    // 見えている画面外が広がるような移動は禁止する：
+    //
+    // - 印字は動かない、プレイヤーの移動量を見ること。
+    // TODO: プレイヤーの移動量と、印字の移動量を分けれないか？
+    //
+    // Printing
+    // +------------------------------->+     c  pd
+    // ^                                |     ^
+    // |       Board                    |     |
+    // |       +------------------+     | 0   +
+    // |       ^//////////////////|     |
+    // |       |///+-------+//////|     |
+    // |       |///|       |//////|     |
+    // |       |///|       |//////|     |
+    // ch      bh//|   *   |//////|     |
+    // |       |///|       |//////|     |
+    // |       |///|       |//////|     |
+    // |       |///+-------+//////|     |
+    // |       |//////////////////|     |
+    // |       v//////////////////|     |
+    // |       +------------------+     +
+    // |                                ^
+    // |                                |
+    // |                                m
+    // |                                |
+    // v                                v
+    // +--------------------------------+
+    //
+    // 0 は、盤の初期位置からの移動量。盤は移動しないので常に 0。
+    // c は、0 からみた、印字の上隅位置。 初期値は習慣的に、 0 以下にするものと思われる。
+    // bh は、盤の行数。
+    // ch は、印字の行数。
+    // m が、マスクの横幅（下側の多めの１を含まない）より小さいなら、それ以上下に行くことはできない。
+    //
+    // m = ch + c - bh
+    //
+
+    const pd = -(getPrinting1RankDelta(printing1Top, board1SquareHeight) + 1);  // まだ +1 （下へ移動）されていないので、+1 しておく。
+    const ch = printing1RankNum; // 例えば 10
+    const bh = board1RankNum;
+    const m = ch + pd - bh;
+    //console.log(`[m=${m}] = [ch=${ch}] + [pd=${pd}] - [bh=${bh}].  m <= -board1WithMaskHeight:${m <= -board1WithMaskSizeSquare}`);
+    return m < -board1WithMaskSizeSquare;
+}
