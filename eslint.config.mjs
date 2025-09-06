e57df9f80ebf205877abf212d9eb22d5062d6295
muzudho <muzudho1@gmail.com>
2025-09-07 03:25:03 +0900

    eslint.config.mjs を作ってみる

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from "eslint/config";
+import vue from "eslint-plugin-vue";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("plugin:vue/vue3-recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        vue,
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+        },
+    },
+
+    rules: {
+        "vue/multi-word-component-names": "off",
+        "vue/valid-v-slot": "error",
+    },
+}]);
