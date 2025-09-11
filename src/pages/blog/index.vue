5d24ad9819170f79dd731a990909a8aa0f0bf1e2
muzudho <muzudho1@gmail.com>
2025-09-11 20:40:12 +0900

    ヘッダーとフッター　＞　Blog

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,25 +1,7 @@
 <template>
-    <the-app-header/>
-    <the-parts-header/>
-
-    <h2>ブログだぜ！　の部　インデックス</h2>
-    <section class="sec-2">
-        上の方の記事が新しいぜ（＾▽＾）！<br/>
-    </section>
-
-    <!--
-        静的インポート・動的コンポーネント
-        router/page-map.ts, router/articles.json も編集してください（TODO: ここらへん自動生成化したい）
-    -->
-    <blog-article page="2025-09/11-thu"/>
-    <blog-article page="2025-09/10-wed"/>
-
-    <!-- 静的インポート・静的コンポーネント -->
-    <Blog20250403ThuSample />
-    <Blog20250402WedSample />
-    <Blog20250401TueSample />
-
-    <the-parts-footer/>
+    <component :is="TheChaptersHeader"/>
+    <component :is="TheChaptersBody"/>
+    <component :is="TheChaptersFooter"/>
 </template>
 
 <script setup lang="ts">
@@ -28,16 +10,12 @@
     // # インポート #
     // ##############
 
-    // ++++++++++++++++++++++++++++++++++
-    // + インポート　＞　コンポーネント +
-    // ++++++++++++++++++++++++++++++++++
+    // ++++++++++++++++++++++++++
+    // + インポート　＞　ページ +
+    // ++++++++++++++++++++++++++
 
-    import BlogArticle from '@/components/BlogArticle.vue';
-    import TheAppHeader from '../../the-app-header.vue';
-    import ThePartsFooter from '../the-parts-footer.vue';
-    import ThePartsHeader from '../the-parts-header.vue';
-    import Blog20250401TueSample from './2025-04/01-tue-sample.vue';
-    import Blog20250402WedSample from './2025-04/02-wed-sample.vue';
-    import Blog20250403ThuSample from './2025-04/03-thu-sample.vue';
+    import TheChaptersBody from '@/pages/blog/the-chapters-body.vue';
+    import TheChaptersFooter from '@/pages/blog/the-chapters-footer.vue';
+    import TheChaptersHeader from '@/pages/blog/the-chapters-header.vue';
 
 </script>
---
+++ src/pages/blog/the-chapters-body.vue
@@ -0,0 +1,40 @@
+<template>
+    <h2>ブログだぜ！　の部　インデックス</h2>
+    <section class="sec-2">
+        上の方の記事が新しいぜ（＾▽＾）！<br/>
+    </section>
+
+    <!--
+        静的インポート・動的コンポーネント
+        router/page-map.ts, router/articles.json も編集してください（TODO: ここらへん自動生成化したい）
+    -->
+    <blog-article page="2025-09/11-thu"/>
+    <blog-article page="2025-09/10-wed"/>
+
+    <!-- 静的インポート・静的コンポーネント -->
+    <Blog20250403ThuSample />
+    <Blog20250402WedSample />
+    <Blog20250401TueSample />
+</template>
+
+<script setup lang="ts">
+
+    // ##############
+    // # インポート #
+    // ##############
+
+    // ++++++++++++++++++++++++++++++++++
+    // + インポート　＞　コンポーネント +
+    // ++++++++++++++++++++++++++++++++++
+
+    import BlogArticle from '@/components/BlogArticle.vue';
+
+    // ++++++++++++++++++++++++++
+    // + インポート　＞　ページ +
+    // ++++++++++++++++++++++++++
+
+    import Blog20250401TueSample from './2025-04/01-tue-sample.vue';
+    import Blog20250402WedSample from './2025-04/02-wed-sample.vue';
+    import Blog20250403ThuSample from './2025-04/03-thu-sample.vue';
+
+</script>
