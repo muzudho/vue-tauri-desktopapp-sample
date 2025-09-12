f2c70348560e41b0f643939670c23cb624ab3577
muzudho <muzudho1@gmail.com>
2025-09-12 18:16:31 +0900

    WIP: アプリ・バー考え中

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,47 @@
 <template>
     <v-app>
         <v-app-bar app density="compact" class="app-bar">
-            <v-toolbar-title>Vue + Tauri デスクトップアプリ・サンプル</v-toolbar-title>
+            <v-toolbar-title style="display: inline;">デスクトップアプリ</v-toolbar-title>
+            <!-- WIP:
+            <v-spacer></v-spacer>
+            <v-btn>画面調整</v-btn>
+            <v-btn>ゲーム調整</v-btn>
+            -->
+        </v-app-bar>
+        <v-main>
+            <!-- WIP: パンくずリスト -->
+            <!--
+div.app-bar-breadcrumb-list {
+    display: inline-block;
+    left: 0;
+    right: 0;
+    width: 98%;
+    margin-left: 1%;
+    margin-top:4px;
+    margin-bottom:4px;
+    padding-left: 8px;
+    padding-top: 1px;
+    padding-right: 8px;
+    padding-bottom: 2px;
+    border-radius: 15px;
+    color: burlywood;
+    background-color: maroon;
+}
+button.app-bar-button {
+    min-width: 20px;
+    max-width: 20px;
+    max-height: 20px;
+    padding-left: 4px;
+    padding-right: 4px;
+    background-color: white;
+}
+
             <div class="app-bar-breadcrumb-list">
                 📁&nbsp;
                 <v-btn class="app-bar-button">❌</v-btn>&nbsp;リファレンス&nbsp;＞&nbsp;
                 <v-btn class="app-bar-button">❌</v-btn>&nbsp;メイキング
             </div>
-            <v-spacer></v-spacer>
-            <v-btn>画面調整</v-btn>
-            <v-btn>ゲーム調整</v-btn>
-        </v-app-bar>
-        <v-main>
+            -->
 
             <!-- Nuxt + Vue ウェブアプリ用：
                 <v-container> は：
