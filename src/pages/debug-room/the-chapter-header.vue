24cd53d287c0fb975c487f06d978e9c422880d4b
muzudho <muzudho1@gmail.com>
2025-09-12 20:45:20 +0900

    ButtonToClosePane

--- src/pages/debug-room/the-chapter-header.vue
+++ src/pages/debug-room/the-chapter-header.vue
@@ -1,6 +1,8 @@
 <template>
     <the-app-header/>
-    <button-to-close-below-part
+    <button-to-close-pane
+        class="sec-0"
+        direction="down"
         pagePath="/"
     />
     <button-to-close-below-chapter
@@ -19,7 +21,7 @@
     // ++++++++++++++++++++++++++++++++++
 
     import ButtonToCloseBelowChapter from '@/components/ButtonToCloseBelowChapter.vue';
-    import ButtonToCloseBelowPart from '@/components/ButtonToCloseBelowPart.vue';
+    import ButtonToClosePane from '@/components/ButtonToClosePane.vue';
     import TheAppHeader from '../the-app-header.vue';
 
 </script>
