8500e4e2f744a660bdbefb1de27f8d41c49b627c
muzudho <muzudho1@gmail.com>
2025-09-17 08:57:07 +0900

    WIP: ゲームマシン　＞　コンポーネント化

--- src/pages/reference/making/input-axis/new-board-3.vue
+++ src/pages/reference/making/input-axis/new-board-3.vue
@@ -4,11 +4,6 @@
 
 <template>
 
-    <!-- 機能 -->
-    <button-20250822 ref="button1Ref"/>
-
-    <compatible-device ref="compatibleDevice1Ref"/>
-
     <!-- ゲームマシン -->         
     <game-machine-waratch2
         :hardPositionStyle="{
@@ -35,9 +30,6 @@
     // # インポート #
     // ##############
 
-    import { ref, watch } from 'vue';
-    // 👆 ［初級者向けのソースコード］では、 reactive は使いません。
-
     // ++++++++++++++++++++++++++++++++++
     // + インポート　＞　コンポーネント +
     // ++++++++++++++++++++++++++++++++++
@@ -46,77 +38,7 @@
     //
 
     // アルファベット順
-    import Button20250822 from '@/components/Button20250822.vue';
-    import CompatibleDevice from '@/components/CompatibleDevice.vue'
     import GameMachineWaratch2 from '@/components/GameMachineWaratch2.vue';
-
-    // +++++++++++++++++++++++++++++++++++
-    // + インポート ＞　インターフェース +
-    // +++++++++++++++++++++++++++++++++++
-
-    import type Rectangle from '../../../../interfaces/Rectangle';
-
-
-    // ################
-    // # オブジェクト #
-    // ################
-
-    // ++++++++++++++++++++++++++++++++++
-    // + オブジェクト　＞　装置の互換性 +
-    // ++++++++++++++++++++++++++++++++++
-
-    const compatibleDevice1Ref = ref<InstanceType<typeof CompatibleDevice> | null>(null);
-
-    // ++++++++++++++++++++++++++++++++
-    // + オブジェクト　＞　ボタン拡張 +
-    // ++++++++++++++++++++++++++++++++
-
-    const button1Ref = ref<InstanceType<typeof Button20250822> | null>(null);
-
-    // ++++++++++++++++++++++++++++++++
-    // + オブジェクト　＞　タイル盤１ +
-    // ++++++++++++++++++++++++++++++++
-
-    const tileBoard1TileWidth = 32;
-    const tileBoard1TileHeight = 32;
-    const board1FileNum = ref<number>(5);   // 筋の数。ただし、右側と下側に１マス余分に付いているマスクは含まない。
-    // ※　盤およびその各タイルは、決まりきった位置でラップアラウンドを繰り返すだけです。座標が大きく移動することはありません。
-    const board1FloorTilemapTileNum = 5;  // 床のタイルマップの、左上隅から数えたタイル数
-
-    // ++++++++++++++++++++++++++++
-    // + オブジェクト　＞　像盤１ +
-    // ++++++++++++++++++++++++**++
-    //
-    // 盤上に表示される数字柄、絵柄など。
-    //
-
-    const printing1OutOfSightIsLock = ref<boolean>(false);   // ［画面外隠し］を管理（true: ロックする, false: ロックしない）
-    watch(printing1OutOfSightIsLock, (newValue: boolean)=>{
-        player1CanBoardEdgeWalkingIsEnabled.value = newValue;
-    });
-    const printing1FileMax = 10;    // 印字の最大サイズは、盤のサイズより大きいです。
-    const printing1RankMax = 10;
-    const printing1AreaMax = printing1FileMax * printing1RankMax;
-    const imageBoard1Data = ref<number[]>([]);   // ソース・タイルのインデックスが入っている盤
-    // ランダムなマップデータを生成
-    for (let i=0; i<printing1AreaMax; i++) {    // 最初から最大サイズで用意します。
-        // 左上のタイルは画面外の黒なので、それを避けて設定。
-        const sourceTileIndex = Math.floor(Math.random() * (board1FloorTilemapTileNum - 1)) + 1;
-        imageBoard1Data.value.push(sourceTileIndex);
-    }
-    const sourceTilemapRectangles : Rectangle[] = [];
-    for (let i = 0; i < printing1AreaMax; i++) {   // 最大サイズで作っておく。
-        const files = i % board1FileNum.value;
-        const ranks = Math.floor(i / board1FileNum.value);
-        sourceTilemapRectangles.push({ top: ranks * tileBoard1TileHeight, left: files * tileBoard1TileWidth, width: tileBoard1TileWidth, height: tileBoard1TileHeight });
-    }
-
-    // ++++++++++++++++++++++++++++
-    // + オブジェクト　＞　自機１ +
-    // ++++++++++++++++++++++++++++
-
-    // アニメーションのことを考えると、 File, Rank ではデジタルになってしまうので、 Left, Top で指定したい。
-    const player1CanBoardEdgeWalkingIsEnabled = ref<boolean>(false);    // ［盤の端の歩行］可能状態の活性性を管理（true: 不活性にする, false: 活性にする）
 
 
     // ################
@@ -178,9 +100,3 @@
     }
 
 </script>
-
-<style scoped>
-    @import '@/styles/talk-scene.css';
-    @import '@/styles/perspective.css';
-    @import '@/styles/game-machine-waratch2.css';
-</style>
