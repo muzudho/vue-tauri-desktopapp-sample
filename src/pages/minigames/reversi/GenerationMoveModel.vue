44c09670ef0f5a8478c3f7f5a1f921894c615c0a
muzudho <muzudho1@gmail.com>
2025-10-26 20:20:52 +0900

    リバーシ＞指し手生成＞コード掃除

--- src/pages/minigames/reversi/GenerationMoveModel.vue
+++ src/pages/minigames/reversi/GenerationMoveModel.vue
@@ -11,7 +11,7 @@
     import type {Color, Direction} from '@/pages/minigames/reversi/spec.ts';
     import {
         // 色
-        COLOR_EMPTY, COLOR_BLACK, COLOR_WHITE, COLOR_SIZE,
+        COLOR_EMPTY,
 
         // 方向
         DIRECTION_SIZE,
@@ -22,14 +22,10 @@
     // # オブジェクト #
     // ################
 
-    // FIXME: Direction別にする必要があるのでは？
-    const gameBoard1CanMove = ref<boolean[][][]>( // [Direction][Color][Square]
+    // FIXME: 値を Color 型にすべきでは。 `[Direction][Color][Square] = boolean` ではなく、 `[Direction][Square] = Color` で十分
+    const gameBoard1ValidNextColor = ref<Color[][]>(
         new Array(DIRECTION_SIZE)
     );
-    // // FIXME: 値を Color 型にすべきでは。 `[Direction][Color][Square] = boolean` ではなく、 `[Direction][Square] = Color` で十分
-    // const gameBoard1ValidNextColor = ref<Color[][]>(
-    //     new Array(DIRECTION_SIZE)
-    // );
 
 
     // ################
@@ -46,11 +42,7 @@
         gameBoard1Area: number,
     ) : void {
         for (const direction of activeDirections) {
-            gameBoard1CanMove.value[direction] = new Array<boolean[]>(COLOR_SIZE);
-            gameBoard1CanMove.value[direction][COLOR_BLACK] = new Array<boolean>(gameBoard1Area).fill(false);
-            gameBoard1CanMove.value[direction][COLOR_WHITE] = new Array<boolean>(gameBoard1Area).fill(false);
-
-            // gameBoard1ValidNextColor.value[direction] = new Array<Color>(gameBoard1Area).fill(COLOR_EMPTY);
+            gameBoard1ValidNextColor.value[direction] = new Array<Color>(gameBoard1Area).fill(COLOR_EMPTY);
         }
     }
 
@@ -76,10 +68,11 @@
         return false;
     }
 
+
     /**
-     * 置ける石の色
-     * @param activeDirections 
-     * @param thisTurn 
+     * 次に置ける石
+     * 
+     * @param directions 
      * @param sq 
      * @returns 
      */
@@ -87,73 +80,24 @@
         direction: Direction,
         sq: number
     ) : Color {
-        if (gameBoard1CanMove.value[direction][COLOR_BLACK][sq]) {
-            return COLOR_BLACK;
-        }
-
-        if (gameBoard1CanMove.value[direction][COLOR_WHITE][sq]) {
-            return COLOR_WHITE;
-        }
-
-        return COLOR_EMPTY;
+        return gameBoard1ValidNextColor.value[direction][sq];
     }
-    // /**
-    //  * 次に置ける石
-    //  * 
-    //  * @param directions 
-    //  * @param sq 
-    //  * @returns 
-    //  */
-    // function getValidNextColor(
-    //     direction: Direction,
-    //     sq: number
-    // ) : Color {
-    //     return gameBoard1ValidNextColor.value[direction][sq];
-    // }
 
 
     /**
-     * ［ストーンズ・キャップ］に石を置けるかどうか判定し、更新します
+     * 次に置ける石を設定
      * 
-     * @param generationMoveModel1Ref 
-     * @param direction 
-     * @param colorList ［連続する石］について、その色の並び順リスト
+     * @param directions 
      * @param sq 
+     * @param color
      */
     function setValidNextColor(
         direction: Direction,
         sq: number,
         color: Color,
     ) : void {
-        if (color == COLOR_BLACK) {
-            gameBoard1CanMove.value[direction][COLOR_BLACK][sq] = true;
-            gameBoard1CanMove.value[direction][COLOR_WHITE][sq] = false;
-            return;
-        }
-
-        if (color == COLOR_WHITE) {
-            gameBoard1CanMove.value[direction][COLOR_BLACK][sq] = false;
-            gameBoard1CanMove.value[direction][COLOR_WHITE][sq] = true;
-            return;
-        }
-
-        gameBoard1CanMove.value[direction][COLOR_BLACK][sq] = false;
-        gameBoard1CanMove.value[direction][COLOR_WHITE][sq] = false;
+        gameBoard1ValidNextColor.value[direction][sq] = color;
     }
-    // /**
-    //  * 次に置ける石を設定
-    //  * 
-    //  * @param directions 
-    //  * @param sq 
-    //  * @param color
-    //  */
-    // function setValidNextColor(
-    //     direction: Direction,
-    //     sq: number,
-    //     color: Color,
-    // ) : void {
-    //     gameBoard1ValidNextColor.value[direction][sq] = color;
-    // }
 
 
     // ################
